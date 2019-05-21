f4d5409702415694561e0830933a3a0d47266226
Joy Lin <vvlnote@hotmail.com>
2019-05-21 21:11:58 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   def update
     #raise params.inspect
     @article = Article.find(params[:id])
-    @article = Article.update(:title => params[:title], :description => params[:description])
+    @article = Article.update(title: params[:title], description: params[:description])
     raise @article.inspect
     redirect_to article_path(@article)
   end
